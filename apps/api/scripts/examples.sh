951dfb720ce4b6dafde19c4d97f0f56e342900b5
Omar Aziz <github@omaraziz.dev>
2023-07-01 22:37:40 -0700

    update broken example

--- apps/api/scripts/examples.sh
+++ apps/api/scripts/examples.sh
@@ -1,8 +1,5 @@
-curl --request POST \
-  --url 'http://127.0.0.1:8787/mainnet/0xf4212614C7Fe0B3feef75057E88b2E77a7E23e83' \
-  --header 'content-type: application/json' \
-  --data '["0x7aE1D57b58fA6411F32948314BadD83583eE0e8C"]'
+# make sure dev is running (`pnpm dev`)
 
 curl --request GET \
-  --url 'http://127.0.0.1:8787/mainnet/0xf4212614C7Fe0B3feef75057E88b2E77a7E23e83' \
+  --url 'http://127.0.0.1:8787/chain/arbitrum/address/0xd8da6bf26964af9d7eed9e03e53415d37aa96045' \
   --header 'content-type: application/json'
